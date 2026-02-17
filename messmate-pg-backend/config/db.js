3da084b9192dd70f5fb2a527022d6c04df9a9033
farid <khanfaridkhan9648@gmail.com>
2026-02-17 21:56:37 +0530

    cloud database update

--- messmate-pg-backend/config/db.js
+++ messmate-pg-backend/config/db.js
@@ -1,16 +1,33 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME, 
-  process.env.DB_USER, 
-  process.env.DB_PASSWORD, 
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+const connectionString = process.env.DATABASE_URL;
+
+let sequelize;
+if (connectionString) {
+  sequelize = new Sequelize(connectionString, {
     dialect: 'postgres',
-    logging: false
-  }
-);
+    protocol: 'postgres',
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      dialect: 'postgres',
+      logging: false
+    }
+  );
+}
 
 module.exports = sequelize;
